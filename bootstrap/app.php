316483c3a42f24452cf10f8e745dff3a06b8acf0
lanzxcs <lanzlaurence01@gmail.com>
2026-01-29 13:38:59 +0800

    Install spatie packages

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,6 +21,14 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
+
+        $middleware->alias([
+            'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+            'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class
+        ]);
+
+        $middleware->append(\Spatie\CookieConsent\CookieConsentMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
